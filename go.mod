4fb14324aa40b709e4497e51337e1677e6059a82
Egor Malykh <fnk@fea.st>
2020-02-23 00:33:02 +0300

    implement directories support

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/meownoid/sharpei
 go 1.13
 
 require (
+	github.com/fatih/color v1.9.0
 	github.com/meownoid/stempl v0.0.0-20200215212511-a9fa4c7cafc6
 	github.com/pkg/errors v0.9.1
 	gopkg.in/yaml.v2 v2.2.8
--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
 	"os/user"
 	"path/filepath"
 	"strings"
 
+	col "github.com/fatih/color"
 	"github.com/meownoid/sharpei/vips"
 	"github.com/meownoid/stempl"
 	"github.com/pkg/errors"
@@ -19,6 +20,11 @@ import (
 func usage() {
 	fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [OPTIONS] PATH [PATH] ...\n", os.Args[0])
 	flag.PrintDefaults()
+}
+
+type outputFile struct {
+	buf *bytes.Buffer
+	ext string
 }
 
 func main() {
@@ -33,8 +39,10 @@ func main() {
 
 		width         = flag.Int("width", 0, "width of the output image")
 		height        = flag.Int("height", 0, "height of the output image")
-		inputProfile  = flag.String("input_profile", "", "input icc profile")
-		outputProfile = flag.String("output_profile", "", "output icc profile")
+		inputProfile  = flag.String("input-profile", "", "input icc profile")
+		outputProfile = flag.String("output-profile", "", "output icc profile")
+
+		noColor = flag.Bool("no-color", false, "disable colorized output")
 	)
 
 	flag.Parse()
@@ -42,6 +50,10 @@ func main() {
 	if flag.NArg() == 0 {
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if *noColor {
+		col.NoColor = true
 	}
 
 	var cfg *Config
@@ -103,132 +115,208 @@ func main() {
 		log.Fatal("no config found, use cli arguments, config flag, sharpei.yml or ~/.sharpei.yml")
 	}
 
+	pathsToProcess := make([]string, 0, flag.NArg())
+
+	for _, path := range flag.Args() {
+		stat, err := os.Stat(path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				fmt.Printf("%s: %s\n", path, col.RedString("does not exists, skipping"))
+				continue
+			}
+			log.Fatal(err)
+		}
+
+		if stat.IsDir() {
+			files, err := ioutil.ReadDir(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, file := range files {
+				if !file.IsDir() && isImage(file.Name()) {
+					pathsToProcess = append(pathsToProcess, filepath.Join(path, file.Name()))
+				}
+			}
+			continue
+		}
+
+		if !isImage(path) {
+			fmt.Printf("%s: %s\n", path, col.RedString("not an image, skipping"))
+			continue
+		}
+
+		pathsToProcess = append(pathsToProcess, path)
+	}
+
+	if len(pathsToProcess) == 0 {
+		col.Green("No images to process")
+		os.Exit(0)
+	}
+
 	vips.Init(os.Args[0])
 	defer vips.Shutdown()
 
-	for _, arg := range flag.Args() {
-		err := process(cfg, arg)
-		if err != nil {
-			log.Fatal(err)
-		}
+	for _, inputPath := range pathsToProcess {
+		func(inputPath string) {
+			reader, err := os.Open(inputPath)
+			if err != nil {
+				fmt.Printf("%s: %s\n", inputPath, col.RedString(err.Error()))
+				return
+			}
+			defer reader.Close()
+
+			img, err := vips.Decode(reader)
+			if err != nil {
+				fmt.Printf("%s: %s\n", inputPath, col.RedString(err.Error()))
+			}
+			defer img.Destroy()
+
+			basename := filepath.Base(inputPath)
+			name := strings.TrimSuffix(basename, filepath.Ext(basename))
+
+			for profileName, profile := range cfg.Profiles {
+				func(profileName string, profile ProfileConfig) {
+					out, err := processProfile(profile, img)
+					if err != nil {
+						fmt.Printf("%s: error while processing profile %s: %s\n", inputPath, profileName, col.RedString(err.Error()))
+						return
+					}
+
+					filename, err := stempl.Format(
+						cfg.Format,
+						map[string]string{
+							"profile": profileName,
+							"name":    name,
+						},
+					)
+					if err != nil {
+						fmt.Printf("%s: error in format string for profile %s: %s\n", inputPath, profileName, col.RedString(err.Error()))
+						return
+					}
+
+					filename = fmt.Sprintf("%s.%s", filename, out.ext)
+
+					inputDir := filepath.Dir(inputPath)
+					outputDir := filepath.Join(cfg.Output, inputDir)
+
+					stat, err := os.Stat(outputDir)
+
+					if err != nil {
+						if os.IsNotExist(err) {
+							err = os.MkdirAll(outputDir, 0755)
+						}
+						if err != nil {
+							fmt.Printf("%s: %s\n", outputDir, col.RedString(err.Error()))
+							return
+						}
+					} else if !stat.IsDir() {
+						fmt.Printf("%s: %s\n", outputDir, col.RedString("exists and not a directory, skipping"))
+						return
+					}
+
+					outputPath := filepath.Join(outputDir, filename)
+
+					if _, err := os.Stat(outputPath); err == nil && !cfg.Rewrite {
+						fmt.Printf("%s: %s\n", outputPath, col.RedString("already exists, skipping"))
+						return
+					}
+
+					f, err := os.Create(outputPath)
+					if err != nil {
+						fmt.Printf("%s: %s\n", outputPath, col.RedString(err.Error()))
+						return
+					}
+					defer f.Close()
+
+					_, err = out.buf.WriteTo(f)
+					if err != nil {
+						fmt.Printf("%s: %s\n", outputPath, col.RedString(err.Error()))
+						return
+					}
+
+					fmt.Printf("%s: %s\n", outputPath, col.GreenString("OK"))
+				}(profileName, profile)
+			}
+		}(inputPath)
 	}
 }
 
-func process(cfg *Config, file string) error {
-	reader, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-
-	img, err := vips.Decode(reader)
-	if err != nil {
-		return err
-	}
-	defer img.Destroy()
-
-	for profileName, profile := range cfg.Profiles {
-		transformedImg, err := TransformImage(
-			img,
-			TransfromConfig{
-				Width:         profile.Width,
-				Height:        profile.Height,
-				InputProfile:  profile.InputProfile,
-				OutputProfile: profile.OutputProfile,
-			},
-		)
-		if err != nil {
-			return err
-		}
-		defer transformedImg.Destroy()
-
-		quality := profile.Quality
-		if quality == 0 {
-			quality = 95
-		}
-		if quality < 1 {
-			quality = 1
-		}
-		if quality > 100 {
-			quality = 100
-		}
-
-		compression := profile.Compression
-		if compression == 0 {
-			compression = 7
-		}
-		if compression < 1 {
-			compression = 1
-		}
-		if compression > 9 {
-			compression = 9
-		}
-
-		fileType := strings.ToLower(profile.Type)
-
-		buf := bytes.NewBuffer([]byte{})
-
-		switch fileType {
-		case "jpeg", "jpg", "jpe", "jif", "jfif", "jfi":
-			err = transformedImg.EncodeJPEG(buf, quality)
-		case "png":
-			err = transformedImg.EncodePNG(buf, compression)
-		case "tiff", "tif":
-			err = transformedImg.EncodeTIFF(buf)
-		case "webp":
-			err = transformedImg.EncodeWEBP(buf, quality, false)
-		default:
-			return errors.Errorf("unsupported file type %s, use jpg, png, webp or tiff", fileType)
-		}
-
-		if err != nil {
-			return err
-		}
-
-		random := randString(32)
-
-		basename := filepath.Base(file)
-		name := strings.TrimSuffix(basename, filepath.Ext(basename))
-
-		filename, err := stempl.Format(
-			cfg.Format,
-			map[string]string{
-				"profile":  profileName,
-				"name":     name,
-				"random32": random,
-				"random16": random[:16],
-				"random8":  random[:8],
-				"random4":  random[:4],
-			},
-		)
-		if err != nil {
-			return errors.Wrap(err, "format error")
-		}
-
-		filename = fmt.Sprintf("%s.%s", filename, fileType)
-
-		if _, err := os.Stat(filename); err == nil && !cfg.Rewrite {
-			return errors.Errorf("file %s already exists and rewrite=false", filename)
-		}
-
-		f, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-
-		buf.WriteTo(f)
+func isImage(filename string) bool {
+	switch strings.ToLower(filepath.Ext(filepath.Base(filename))) {
+	case ".jpeg", ".jpg", ".jpe", ".jif", ".jfif", ".jfi":
+		return true
+	case ".png":
+		return true
+	case ".tiff", ".tif":
+		return true
+	case ".webp":
+		return true
 	}
 
-	return nil
+	return false
 }
 
-const randStringBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-func randString(n int) string {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = randStringBytes[rand.Int63()%int64(len(randStringBytes))]
+func processProfile(profile ProfileConfig, img *vips.Image) (*outputFile, error) {
+	transformedImg, err := TransformImage(
+		img,
+		TransfromConfig{
+			Width:         profile.Width,
+			Height:        profile.Height,
+			InputProfile:  profile.InputProfile,
+			OutputProfile: profile.OutputProfile,
+		},
+	)
+	if err != nil {
+		return nil, err
 	}
-	return string(b)
+	defer transformedImg.Destroy()
+
+	quality := profile.Quality
+	if quality == 0 {
+		quality = 95
+	}
+	if quality < 1 {
+		quality = 1
+	}
+	if quality > 100 {
+		quality = 100
+	}
+
+	compression := profile.Compression
+	if compression == 0 {
+		compression = 7
+	}
+	if compression < 1 {
+		compression = 1
+	}
+	if compression > 9 {
+		compression = 9
+	}
+
+	fileType := strings.ToLower(profile.Type)
+
+	buf := bytes.NewBuffer([]byte{})
+
+	switch fileType {
+	case "jpeg", "jpg", "jpe", "jif", "jfif", "jfi":
+		err = transformedImg.EncodeJPEG(buf, quality)
+	case "png":
+		err = transformedImg.EncodePNG(buf, compression)
+	case "tiff", "tif":
+		err = transformedImg.EncodeTIFF(buf)
+	case "webp":
+		err = transformedImg.EncodeWEBP(buf, quality, false)
+	default:
+		return nil, errors.Errorf("unsupported file type %s, use jpg, png, webp or tiff", fileType)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &outputFile{
+		buf: buf,
+		ext: fileType,
+	}, nil
 }
