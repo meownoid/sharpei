1be66904ddebef48df02fab4f1476c095cc1530b
Egor Malykh <fnk@fea.st>
2020-02-24 02:54:58 +0300

    implement better icc profile options handling

--- transform.go
+++ transform.go
@@ -50,7 +50,6 @@ type TransfromConfig struct {
 }
 
 func TransformImage(img *vips.Image, cfg TransfromConfig) (*vips.Image, error) {
-	// Check config
 	if cfg.Width < 0 {
 		cfg.Width = 0
 	}
@@ -68,6 +67,18 @@ func TransformImage(img *vips.Image, cfg TransfromConfig) (*vips.Image, error) {
 	scaley := float64(cfg.Height) / float64(img.Height())
 	scale := math.Max(scalex, scaley)
 
+	isEmbeddedICC := img.IsPropertySet("icc-profile-data")
+
+	if cfg.OutputProfile == "same" && isEmbeddedICC {
+		// Resize image in the original color space
+		imgResized, err := img.Resize(scale, scale)
+		if err != nil {
+			return nil, err
+		}
+
+		return imgResized, nil
+	}
+
 	if cfg.InputProfile == "" {
 		switch img.Interpretation() {
 		case vips.INTERPRETATION_B_W, vips.INTERPRETATION_GREY16:
@@ -77,71 +88,70 @@ func TransformImage(img *vips.Image, cfg TransfromConfig) (*vips.Image, error) {
 		}
 	}
 
-	isEmbeddedICC := img.IsPropertySet("icc-profile-data")
+	imgWithICCProfile := img
+	profileAttached := ""
 
-	var imgExported *vips.Image
-	var err error
+	// If image has no attached profile, load default one and embedd it into the image
+	if !isEmbeddedICC {
+		profileAttached = cfg.InputProfile
 
-	if cfg.OutputProfile == "same" && isEmbeddedICC {
-		// Resize image in the original color space
-		imgExported, err = img.Resize(scale, scale)
+		inputProfile, err := getProfile(cfg.InputProfile)
 		if err != nil {
 			return nil, err
 		}
-	} else {
-		imgWithICCProfile := img
 
-		// If image has no attached profile, load default one and embedd it into the image
-		if !isEmbeddedICC {
-			inputProfile, err := getProfile(cfg.InputProfile)
-			if err != nil {
-				return nil, err
-			}
-
-			imgWithICCProfile, err = img.Copy()
-			if err != nil {
-				return nil, err
-			}
-			defer imgWithICCProfile.Destroy()
-
-			imgWithICCProfile.SetPropertyBlob("icc-profile-data", inputProfile)
-		}
-
-		// Import image to LAB PCS space using embedded profile
-		imgImported, err := imgWithICCProfile.ICCImport(vips.INTENT_RELATIVE)
+		imgWithICCProfile, err = img.Copy()
 		if err != nil {
 			return nil, err
 		}
-		defer imgImported.Destroy()
+		defer imgWithICCProfile.Destroy()
 
-		// Resize image in the LAB PCS space
-		imgResized, err := imgImported.Resize(scale, scale)
+		imgWithICCProfile.SetPropertyBlob("icc-profile-data", inputProfile)
+	}
+
+	// Import image to LAB PCS space using embedded profile
+	imgImported, err := imgWithICCProfile.ICCImport(vips.INTENT_RELATIVE)
+	if err != nil {
+		return nil, err
+	}
+	defer imgImported.Destroy()
+
+	// Resize image in the LAB PCS space
+	imgResized, err := imgImported.Resize(scale, scale)
+	if err != nil {
+		return nil, err
+	}
+	defer imgResized.Destroy()
+
+	imgResizedCopy, err := imgResized.Copy()
+	if err != nil {
+		return nil, err
+	}
+	defer imgResizedCopy.Destroy()
+
+	if cfg.OutputProfile == "" || cfg.OutputProfile == "same" {
+		switch img.Interpretation() {
+		case vips.INTERPRETATION_B_W, vips.INTERPRETATION_GREY16:
+			cfg.OutputProfile = "gray"
+		default:
+			cfg.OutputProfile = "srgb"
+		}
+	}
+
+	// Load output profile and attach it to the image
+	if cfg.OutputProfile != profileAttached {
+		outputProfile, err := getProfile(cfg.OutputProfile)
 		if err != nil {
 			return nil, err
 		}
-		defer imgResized.Destroy()
 
-		imgResizedCopy, err := imgResized.Copy()
-		if err != nil {
-			return nil, err
-		}
-		defer imgResizedCopy.Destroy()
+		imgResizedCopy.SetPropertyBlob("icc-profile-data", outputProfile)
+	}
 
-		// Load output profile and attach it to the image
-		if cfg.OutputProfile != "same" && cfg.OutputProfile != "" {
-			outputProfile, err := getProfile(cfg.OutputProfile)
-			if err != nil {
-				return nil, err
-			}
-
-			imgResizedCopy.SetPropertyBlob("icc-profile-data", outputProfile)
-		}
-
-		// Export image to output ICC profile
-		imgExported, err = imgResizedCopy.ICCExport(vips.INTENT_RELATIVE, 8)
-		if err != nil {
-			return nil, err
-		}
+	// Export image to output ICC profile
+	imgExported, err := imgResizedCopy.ICCExport(vips.INTENT_RELATIVE, 8)
+	if err != nil {
+		return nil, err
 	}
 
 	return imgExported, nil
